4145eff5d56a9850c7a00fdc2e0705a43ca70dc8
yogesy362 <yogesy362@gmail.com>
2026-09-03 11:14:19 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -11,3 +11,18 @@
 -- DepartmentID
 
 -- Add constraints
+CREATE DATABASE  college;
+
+use college;
+
+CREATE TABLE student(
+   studentID INT(5)PRIMARY KEY,
+   studentName VARCHAR(20)NOT NULL,
+   DOB DATE NOT NULL,
+   Gender VARCHAR(10)NOT NULL,
+   DepartmentID INT(50)NOT NULL
+   );Create database college_db;
+use college_db;
+CREATE TABLE department (department_id INT PRIMARY KEY, department_name VARCHAR(100), HOD VARCHAR(100));
+SELECT* FROM DEPARTMENT;
+ 
