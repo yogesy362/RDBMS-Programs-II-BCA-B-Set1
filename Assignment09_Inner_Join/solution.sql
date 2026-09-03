ae90e6aa29d57c8beb3b3c85fcfab643b020f428
yogesy362 <yogesy362@gmail.com>
2026-09-03 11:23:32 +0530

    Update solution.sql

--- Assignment09_Inner_Join/solution.sql
+++ Assignment09_Inner_Join/solution.sql
@@ -5,3 +5,16 @@
 -- Insert sample records
 
 -- INNER JOIN query
+Create Student and Department tables with the following sample values:
+Department Table
+DepartmentID DepartmentName
+101 Computer Science
+102 Mathematics
+103 Physics
+Student Table
+StudentID StudentName DepartmentID
+1001 Arun 101
+1002 Divya 102
+1003 Karthik 101
+1004 Nisha 103
+Perform an INNER JOIN to display Student Name with Department Name.
