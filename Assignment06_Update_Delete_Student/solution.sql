c0ae5345666a85b2aecd58ace429882538063077
yogesy362 <yogesy362@gmail.com>
2026-09-03 11:19:39 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -9,3 +9,14 @@
 -- Delete StudentID 1002
 
 -- Display all records
+-- Update Karthik's department from 101 to 103
+UPDATE Student
+SET DepartmentID = 103
+WHERE StudentName = 'Karthik';
+
+-- Delete the student record with StudentID 1002
+DELETE FROM Student
+WHERE StudentID = 1002;
+
+-- Display the updated table
+SELECT * FROM Student;
